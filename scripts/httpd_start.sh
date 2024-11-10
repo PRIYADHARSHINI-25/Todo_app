64dcf0ef0a790fd594739a883bd73d24a6a96d2a
PRIYADHARSHINI-25 <83507058+PRIYADHARSHINI-25@users.noreply.github.com>
2024-11-10 23:44:07 +0530

    Update httpd_start.sh

--- scripts/httpd_start.sh
+++ scripts/httpd_start.sh
@@ -2,6 +2,5 @@
 systemctl start httpd
 systemctl enable httpd
 cd /var/www/html/todo
-yum install python3-pip -y
-python3.7 -m pip install flask
-python3.7 app.py
+nohup python3 app.py > app.log 2>&1 &
+
